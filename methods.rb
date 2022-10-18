9e2dee3637e193c8c0e5112288f01fc2acb0dfd8
kimuit117 <kemoikiuita@gmail.com>
2022-10-19 01:18:25 +0300

    completed test

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+# greet_programmer
+
+def greet(name)
+puts "Hello, #{name}!"
+end
+# greet
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+# greet_with_default
+
+def add(num1, num2)
+    return num1 +num2;
+end
+# add
+
+
+def halve(number)
+if number.class != Integer
+     nil
+else
+number/2
+end
+# halve
+end
+
